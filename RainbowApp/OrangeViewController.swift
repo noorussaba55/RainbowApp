93024947abdb2043c83e9c41d6ca0f5d06e17e8f
Noor Karim <noorussabak@gmail.com>
2021-08-12 15:58:30 +1000

    first version
    Tab bar controller

---
+++ RainbowApp/OrangeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrangeViewController.swift
+//  RainbowApp
+//
+//  Created by Noor-us-saba Karim on 12/8/21.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
